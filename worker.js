525e58c6186de05a33a72daab8960882c66d4639
trbKnl <ncdeschipper@gmail.com>
2022-09-27 20:28:50 +0200

    added instagram DDP checking, and added package install with micropip

--- worker.js
+++ worker.js
@@ -6,11 +6,25 @@
 // Setup your project to serve `py-worker.js`. You should also serve
 // `pyodide.js`, and all its associated `.asm.js`, `.data`, `.json`,
 // and `.wasm` files as well:
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
 
 async function loadPyodideAndPackages() {
   self.pyodide = await loadPyodide();
   await self.pyodide.loadPackage(["pandas"]);
+
+  await pyodide.loadPackage("micropip");
+
+  await self.pyodide.runPythonAsync(`
+    import micropip
+    await micropip.install("/ddpinspect-0.0.0-py3-none-any.whl", deps=False)
+  `);
+  //let response = await fetch("/ddpinspect-0.0.0.tar.gz"); // .zip, .whl, ...
+  //let buffer = await response.arrayBuffer()
+  //console.log("check 1")
+  //pyodide.FS.writeFile("/ddpinspect-0.0.0.tar.gz", buffer)//, { encoding: "utf8" });
+  //console.log("check 2")
+  //await micropip.install("emfs:/ddpinspect-0.0.0.tar.gz");
+  //console.log("check 3")
 }
 
 let pyodideReadyPromise = loadPyodideAndPackages();
